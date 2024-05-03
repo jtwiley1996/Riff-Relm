287e304d316a06a05b65da8721b11b0266c2a018
Keely <keelygyork@gmail.com>
2024-05-02 19:37:09 -0500

    modified:   client/src/Main.jsx

---
+++ client/src/Main.jsx
@@ -0,0 +1,22 @@
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import App from './App.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    errorElement: <NotFound />,
+    children: [
+      {
+        index: true,
+        element: <Home />
+      },
+    ],
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+);
