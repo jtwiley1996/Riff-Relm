ed788db0362a6f848d82ed91b1eaf4a5b90ca5eb
gwennr-t <gwennt321@gmail.com>
2024-05-02 21:22:14 -0500

    Added in query and mutation typedefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,17 +1,32 @@
 const typeDefs = `
-type User {
-  _id: ID!
-  username: String!
-  email: String!
-  password: String!
-  createdAt: Date
-  genres: [String!]
-}
+  type User {
+    _id: ID!
+    username: String!
+    email: String!
+    password: String!
+    createdAt: Date
+    genres: [String!]
+  }
 
-type: Auth {
-  token: ID!
-  profile: Profile
-}
-`
+  type: Auth {
+    token: ID!
+    user: User
+  }
+
+  type Query {
+    users: [User]!
+    user(userId: ID!): User
+    me: User
+  }
+
+  type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email: String!, password: String!): Auth
+
+    addSkill(userId: ID!, skill: String!): User
+    removeUser: User
+    removeSkill(skill: String!): User
+  }
+`;
 
 module.exports = typeDefs;
