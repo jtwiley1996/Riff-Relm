5e2fd2b57d237e5bfbdccb3c8d9b06723664439e
Cameron Fair <cameronfaircellist@gmail.com>
2024-05-02 21:18:57 -0500

    resolver user mod

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,13 +1,38 @@
 const { User } = require('../models');
+const { signToken, AuthenticationError } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-        user: async () => {
-            return User.find({});
-          },
+        users: async () => {
+            return User.find();
+        },
+
+        user: async (parent, { profileId }) => {
+            return User.findOne({ _id: profileId });
+        },
+        me: async (parent, args, context) => {
+            if (context.user) {
+                return User.findOne({ _id: context.user._id });
+            }
+            throw AuthenticationError;
+        },
     },
     Mutation: {
-
+        user: async (parent, { profileId }) => {
+            return Profile.findOne({ _id: profileId });
+        },
+        me: async (parent, args, context) => {
+            if (context.user) {
+                return User.findOne({ _id: context.user._id });
+            }
+            throw AuthenticationError;
+        },
+        removeProfile: async (parent, args, context) => {
+            if (context.user) {
+                return Profile.findOneAndDelete({ _id: context.user._id });
+            }
+            throw AuthenticationError;
+        },
     }
 };
 
